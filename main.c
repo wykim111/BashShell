08d80f777147c9485997645024877e25d7479e0c
wykim111 <ksq111@naver.com>
2020-07-15 15:57:24 +0900

    test

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "./main.h"
+
+int main(int argc,char* argv[])
+{
+    char InputStr1[100], InputStr2[100];
+    int stateCmp =  0;
+
+    memset(InputStr1,0,sizeof(InputStr1));
+    memset(InputStr2,0,sizeof(InputStr2));
+
+    scanf("%s %s",InputStr1,InputStr2);
+
+    stateCmp = Cstrcmp(InputStr1,InputStr2);
+
+    if(stateCmp > 0)
+    {
+        printf("%s\n",InputStr1);
+    }
+    else if(stateCmp < 0)
+    {
+       printf("%s\n",InputStr2);
+
+    }
+    else
+    {
+        printf("Same\n");
+    }
+
+
+    return 0;
+}
