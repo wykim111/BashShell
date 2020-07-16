a33b5aae872f496e68803f85d51fa1255966e526
wykim111 <ksq111@naver.com>
2020-07-16 16:22:18 +0900

    헤더파일정의

---
+++ include/StringLib.h
@@ -0,0 +1,6 @@
+#ifndef _StringLib_H_
+#define _StringLib_H_
+
+int Cstrcmp(const char* pstr1,const char* pstr2);
+
+#endif
---
+++ include/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include<stdio.h>
+#include<string.h>
+#include<memory.h>
+
+#include"./StringLib.h"
+
+#endif
